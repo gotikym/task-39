7f93805bd2982005dc3b8743035e8a5e487f3d46
gotikym <gotikym9@mail.ru>
2022-09-13 00:27:21 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Player[] players = { new Player("Ivan", 27), new Player("Mark", 18), new Player("Denis", 32) };
+        
+        ShowPlayers(players);
+    }
+
+    static void ShowPlayers(Player[] players)
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            players[i].ShowInfo();
+        }
+    }
+}
+
+class Player
+{
+    private string _name;
+    private int _age;
+
+    public Player(string name, int age)
+    {
+        _name = name;
+        _age = age;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine("Имя: " + _name + "   Возраст: " + _age);
+    }
+}
